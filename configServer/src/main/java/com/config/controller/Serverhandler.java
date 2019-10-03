b5d1791fd41c91474810fccdcdca4556d580013d
yuren <641864981@qq.com>
2019-10-03 20:03:44 +0800

    远程访问配置文件

---
+++ configServer/src/main/java/com/config/controller/Serverhandler.java
@@ -0,0 +1,19 @@
+package com.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Serverhandler {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index(){
+        return "当前端口为： " +this.port;
+    }
+}
